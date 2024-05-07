3dbc4c780ee07282247e131d336d0150bd826da5
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-05-07 11:32:26 +0600

    adminController code cleaned

--- support-panel-sandbox/app/Http/Controllers/AdminController.php
+++ support-panel-sandbox/app/Http/Controllers/AdminController.php
@@ -2,27 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\DummyCollection;
-use App\Models\Dummy;
-use App\Models\User;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\DB;
-use Illuminate\View\View;
-use Inertia\Inertia;
-use Inertia\Response;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        //$dummy = DummyController::class->index();
-        //$dummyAll = Dummy::select('')->get();
         $dummyData = DB::table('dummies')->paginate(3);
-        //$dummy = DummyResource::collection($dummyAll);
-        //$dummyData = new DummyCollection(Dummy::all());
-        //$dummyData = new DummyCollection(Dummy::all());
-
-        //dd($dummyData);
 
         return inertia(
             'Admin/AdminPage',
